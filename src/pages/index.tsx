9e74cbc9fc5c2546d86920dc44f6da449857966e
helloworld <91911468+helloworld-vol@users.noreply.github.com>
2022-07-29 15:54:58 +0000

    fix: scss を記述できるように修正

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <h1>modern.css</h1>
+      <button>hoge</button>
+    </div>
+  )
+}
+
+export default Home
