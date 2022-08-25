0befc41bbc89e3f64839eda0f0dc10e9a4597c7d
helloworld <91911468+helloworld-vol@users.noreply.github.com>
2022-08-25 14:13:04 +0000

    feat: svg を import 出来るように

--- next.config.js
+++ next.config.js
@@ -2,9 +2,18 @@
 const nextConfig = {
   images: {
     domains: ["nicovideo.cdn.nimg.jp"],
+    disableStaticImages: true,
   },
-  reactStrictMode: true,
   swcMinify: true,
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
